65cf502795fa171a685a41d40d7e690e51217b7f
as6620 <as6620@bs.amalnet.k12.il>
2024-10-26 18:57:08 +0300

    ShoppingCart sum products

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -10,7 +10,12 @@ public class ShoppingCart extends Product{
         this.description = description;
     }
 
-    public int calculateTotal() {
+    public int calculateTotal(){
+        int countPrice;
         Product[] products = new Product[10];
+        for(Product p: products){
+            countPrice += p;
+        }
+        System.out.println("the sum of products is" + countPrice);
     }
 }
